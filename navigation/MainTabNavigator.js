e7663d4776f2b08b46b8693e109773de23888800
Quark Wei <quark.j.wei@gmail.com>
2018-04-07 10:54:39 -0400

    Initial expo-init commit with tiny changes

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import PaymentsScreen from '../screens/PaymentsScreen';
+import ContactsScreen from '../screens/ContactsScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+export default TabNavigator(
+  {
+    Payments: {
+      screen: PaymentsScreen,
+    },
+    Contacts: {
+      screen: ContactsScreen,
+    },
+    Profile: {
+      screen: ProfileScreen,
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Payments':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-cash${focused ? '' : '-outline'}`
+                : 'md-cash';
+            break;
+          case 'Contacts':
+            iconName = Platform.OS === 'ios' ? `ios-contacts${focused ? '' : '-outline'}` : 'md-contacts';
+            break;
+          case 'Profile':
+            iconName =
+              Platform.OS === 'ios' ? `ios-contact${focused ? '' : '-outline'}` : 'md-contact';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3, width: 25 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
